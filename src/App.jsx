8629e25ef4a5f9a5f33369523052c3c6ab80a9be
Alim Shogenov <shogenov@tutu.tech>
2023-01-16 21:27:27 +0300

    added prettier and chart component

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react"
+import Highcharts from "highcharts/highstock"
+import HighchartsReact from "highcharts-react-official"
+import HC_more from "highcharts/highcharts-more" //module
+HC_more(Highcharts) //init module
+
+export const App = () => {
+	const [options] = useState({
+		chart: {
+			events: {
+				load: function () {
+					// set up the updating of the chart each second
+					const series = this?.series?.[0]
+					setInterval(function () {
+						const x = new Date().getTime() // current time
+						const y = Math.round(Math.random() * 100)
+						series.addPoint([x, y], true, true)
+					}, 1000)
+				},
+			},
+			backgroundColor: "#202036",
+			color: "white",
+			type: "spline",
+		},
+
+		time: {
+			useUTC: false,
+		},
+		rangeSelector: {
+			buttons: [
+				{
+					count: 1,
+					type: "minute",
+					text: "1M",
+				},
+				{
+					count: 5,
+					type: "minute",
+					text: "5M",
+				},
+				{
+					type: "all",
+					text: "All",
+				},
+			],
+			inputEnabled: false,
+			selected: 0,
+		},
+
+		title: {
+			text: "Chart in real time",
+		},
+
+		exporting: {
+			enabled: false,
+		},
+
+		series: [
+			{
+				name: "Random data",
+				data: (function () {
+					// generate an array of random data
+					var data = [],
+						time = new Date().getTime(),
+						i
+
+					for (i = -999; i <= 0; i += 1) {
+						data.push([time + i * 1000, Math.round(Math.random() * 100)])
+					}
+					return data
+				})(),
+			},
+		],
+	})
+
+	return (
+		<HighchartsReact
+			constructorType={"stockChart"}
+			highcharts={Highcharts}
+			options={options}
+		/>
+	)
+}
